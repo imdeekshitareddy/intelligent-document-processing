ca93c724133678dc424ca0d2744c250a7e8ab155
imdeekshitareddy <ideekshitareddy@gmail.com>
2026-03-02 18:02:34 +0530

    extraction working with side heading

--- test_pdf.py
+++ test_pdf.py
@@ -1,5 +1,8 @@
 from utils.pdf_reader import extract_text_from_pdf
+from model.entity_extractor import extract_entities
 
 text = extract_text_from_pdf("sample_pdfs/test.pdf")
 
-print(text)
+entities = extract_entities(text)
+
+print(entities)
